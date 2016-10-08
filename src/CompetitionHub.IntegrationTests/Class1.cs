3bd837fafd154f1a16520c4bf07b1530c9a76400
eatsleeptraincode <eatsleeptraincode@gmail.com>
2016-10-08 10:00:34 -0500

    added integration test setup

---
+++ src/CompetitionHub.IntegrationTests/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using CompetitionHub.Mvc;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.PlatformAbstractions;
+using Xunit;
+
+namespace CompetitionHub.IntegrationTests
+{
+    public class Class1
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            var path = PlatformServices.Default.Application.ApplicationBasePath;
+            var setDir = Path.GetFullPath(Path.Combine(path, @"..\..\..\..\CompetitionHub.Mvc" ));
+            var builder = new WebHostBuilder()
+                .UseStartup<Startup>()
+                .UseContentRoot(setDir);
+            var server = new TestServer(builder);
+            var client = server.CreateClient();
+            var response = client.GetAsync("/").Result;
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
